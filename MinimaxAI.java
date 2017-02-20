fea23f25c10e6a3b346756f5a07d2894ff2730f5
Ishan Thapar <ishansthapar@gmail.com>
2017-02-19 21:26:15 -0800

    Created MinimaxAI and AlphaBetaPruningAI classes

---
+++ MinimaxAI.java
@@ -0,0 +1,15 @@
+/**
+ * Authors: Ishan Thapar, 999104208; Samir Rahman, 998988726
+ * AI class that implements Minimax algorithm
+ */
+public class MinimaxAI extends AIModule
+{
+    /**
+     *
+     * @param game Current state of the game.
+     */
+    public void getNextMove(GameStateModule game)
+    {
+
+    }
+}
