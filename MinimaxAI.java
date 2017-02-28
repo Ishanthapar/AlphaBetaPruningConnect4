2da418507305a70e2183f149245a644633706df8
Ishan Thapar <ishansthapar@gmail.com>
2017-02-28 02:04:33 -0800

    Solved issue of passing not updated children states in MinimaxAI.java

--- MinimaxAI.java
+++ MinimaxAI.java
@@ -10,16 +10,21 @@ public class MinimaxAI extends AIModule
     private int maxDepth = 1;
     private int currentPlayer;
     private GameStateModule currentGameState;
+    private GameStateModule gameState;
     private HashMap<GameStateModule, Integer> gameStateMoves;
     private HashMap<Integer, GameStateModule> gameStateValues;
+    //private ArrayList<GameStateModule> children;
+    int [][] board;
 
     //ArrayList<GameStateModule> children;
 
     public void getNextMove(GameStateModule game)
     {
+        board = new int[game.getHeight()][game.getWidth()];
         gameStateMoves = new HashMap<>();
         gameStateValues = new HashMap<>();
         currentGameState = game.copy();
+        gameState = game.copy();
         //currentPlayer = game.getActivePlayer();
         //children = new ArrayList<GameStateModule>();
 
@@ -27,28 +32,28 @@ public class MinimaxAI extends AIModule
         chosenMove = minimax(currentGameState.getActivePlayer(), 0, currentGameState);
     }
 
-    private int evaluateBoard(GameStateModule game)
+    private int evaluateBoard(final GameStateModule game)
     {
         int player = 0;
         int row;
         int column;
         int result = 0;
-        int [][] board = new int[7][6];
+        int [][] board = new int[game.getHeight()][game.getWidth()];
 
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < game.getHeight(); i++)
         {
-            for (int j = 0; j < 6; j++)
+            for (int j = 0; j < game.getWidth(); j++)
             {
-                board[i][j] = game.getAt(i, j);
+                board[i][j] = game.getAt(j, i);
                 //System.out.print(board[i][j] + " ");
             }
             //System.out.println();
         }
         //System.out.println("\n");
 
-        for (int i = 6; i >= 0; i--)
+        for (int i = game.getHeight()-1; i >= 0; i--)
         {
-            for (int j = 5; j >= 0; j--)
+            for (int j = 0; j < game.getWidth(); j++)
             {
                 System.out.print(board[i][j] + " ");
             }
@@ -56,18 +61,24 @@ public class MinimaxAI extends AIModule
         }
         System.out.println("\n");
 
-        System.out.println(board[6][0]);
+        //System.out.println(board[6][0]);
+
 
 
 
         return 0;
     }
 
-    private int minimax(int turn, int depth, GameStateModule currentState)
+    private int minimax(final int turn, final int depth, final GameStateModule currentState)
     {
+        //currentGameState = currentState.copy();
+        //System.out.println("Printing current state of board passed to minimax");
+        //printBoard(currentState);
+
         int bestValue;
         int nodeValue;
         int bestMove;
+        //ArrayList<GameStateModule> myChildren = new ArrayList<>();
 
         if (terminate)
         {
@@ -87,6 +98,9 @@ public class MinimaxAI extends AIModule
             myChildren = saveChildren(currentState);
             for (int i = 0; i < myChildren.size(); i++)
             {
+                //System.out.println("Printing Board");
+                //printBoard(myChildren.get(i));
+                //System.out.println("Done Printing Board");
                 nodeValue = minimax(2, depth + 1, myChildren.get(i));
                 gameStateValues.put(nodeValue, myChildren.get(i));
                 bestValue = Math.max(bestValue, nodeValue);
@@ -98,6 +112,9 @@ public class MinimaxAI extends AIModule
             myChildren = saveChildren(currentState);
             for (int i = 0; i < myChildren.size(); i++)
             {
+                //System.out.println("Printing Board");
+                //printBoard(myChildren.get(i));
+                //System.out.println("Done Printing Board");
                 nodeValue = minimax(1, depth + 1, myChildren.get(i));
                 gameStateValues.put(nodeValue, myChildren.get(i));
                 bestValue = Math.min(bestValue, nodeValue);
@@ -107,23 +124,56 @@ public class MinimaxAI extends AIModule
         return bestMove;
     }
 
-    private ArrayList<GameStateModule> saveChildren(GameStateModule currentState)
+    private ArrayList<GameStateModule> saveChildren(final GameStateModule currentState)
     {
-        int numRows = currentState.getHeight();
         int numColumns = currentState.getWidth();
         ArrayList<GameStateModule> children;
         children = new ArrayList<GameStateModule>();
+        GameStateModule state;
 
         for (int i = 0; i < numColumns; i++)
         {
-            if(currentState.canMakeMove(i))
+            state = currentState.copy();
+            //printBoard(state);
+            if(state.canMakeMove(i))
             {
-                currentState.makeMove(i);
-                children.add(currentState);
-                gameStateMoves.put(currentState, i);
-                currentState.unMakeMove();
+                state.makeMove(i);
+                //currentState.makeMove(i);
+                children.add(state);
+                //printBoard(children.get(i));
+                gameStateMoves.put(state, i);
+                //currentState.unMakeMove();
+                //currentState.unMakeMove();
             }
         }
         return children;
+    }
+
+    private void printBoard(final GameStateModule currentState)
+    {
+
+        //int [][] board = new int[currentState.getHeight()][currentState.getWidth()];
+
+        /*for (int i = 0; i < currentState.getHeight(); i++)
+        {
+            for (int j = 0; j < currentState.getWidth(); j++)
+            {
+                board[i][j] = currentState.getAt(j, i);
+                //System.out.print(board[i][j] + " ");
+            }
+            //System.out.println();
+        }*/
+
+        for (int i = currentState.getHeight()-1; i >= 0; i--)
+        {
+            for (int j = 0; j < currentState.getWidth(); j++)
+            {
+                System.out.print(currentState.getAt(j,i) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+
+        //System.out.println(board[6][0]);
     }
 }
